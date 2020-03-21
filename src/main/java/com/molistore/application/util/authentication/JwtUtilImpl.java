a96b809386f5a8068faded36ba6f68e1cdf69f05
WHALE88 <maksim12288@gmail.com>
2020-03-21 11:54:50 +0200

    moli --> add some documentation about JwtUtil

--- src/main/java/com/molistore/application/util/authentication/JwtUtilImpl.java
+++ src/main/java/com/molistore/application/util/authentication/JwtUtilImpl.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +17,8 @@ import java.util.function.Function;
 
 @Component
 public class JwtUtilImpl implements JwtUtil {
+
+    private final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
 
     @Value("${jwt.secret}")
     private String secretKey;
@@ -31,6 +36,19 @@ public class JwtUtilImpl implements JwtUtil {
         return extractClaim(token, Claims::getExpiration);
     }
 
+
+    /**
+     * The claims tell you, at minimum:
+     *
+     * Who this person is and the URI to their user resource (the sub claim)
+     * What this person can access with this token (the scope claim)
+     * When the token expires. Your API should be using this when it verifies the token.
+     * Because the token is signed with a secret key you can verify its signature and implicitly trust what is being claimed
+     * @param token
+     * @param claimsResolver
+     * @param <T>
+     * @return
+     */
     @Override
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
@@ -38,7 +56,10 @@ public class JwtUtilImpl implements JwtUtil {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        return Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(secretKey))
+                .parseClaimsJws(token)
+                .getBody();
     }
 
     @Override
@@ -52,13 +73,30 @@ public class JwtUtilImpl implements JwtUtil {
         return createToken(claims, userDetails.getUsername());
     }
 
+    /**
+     * Sets the hashing algorithm
+     * Gets the current date for the Issued At claim
+     * Uses the SECRET_KEY static property to generate the signing key
+     * Uses the fluent API to add the claims and sign the JWT
+     * Sets the expiration date
+     * @param claims  The claims tell you, at minimum:
+     * <p> - Who this person is and the URI to their user resource (the sub claims)
+     * <p> - What this person can access with this token (the scope claim)
+     * <p> - When the token expires. Your API should be using this when it verifies the token.
+     * <p>Because the token is signed with a secret key you can verify its signature and implicitly trust what is being claimed
+     * <p>
+     * @param subject userName from {@link UserDetails}
+     * @return Builds the JWT and serializes it to a compact, URL-safe string
+     */
     private String createToken(Map<String, Object> claims, String subject) {
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secretKey);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, SIGNATURE_ALGORITHM.getJcaName());
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(accessTokenExpiration))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(SIGNATURE_ALGORITHM, signingKey)
                 .compact();
     }
 
